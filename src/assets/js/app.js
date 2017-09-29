cacdf7d49298a117ea9f1edae07e811bd4304a2d
Amanda Saeteun <asaeteun@gmail.com>
2017-09-28 18:34:44 -0700

    finished updating error message

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -49,46 +49,16 @@ $('a[href*="#"]')
   }
 });
 
-//Autocomplete
-var substringMatcher = function(strs) {
-  return function findMatches(q, cb) {
-    var matches, substringRegex;
 
-    // an array that will be populated with substring matches
-    matches = [];
-
-    // regex used to determine if a string contains the substring `q`
-    substrRegex = new RegExp(q, 'i');
-
-    // iterate through the pool of strings and for any string that
-    // contains the substring `q`, add it to the `matches` array
-    $.each(strs, function(i, str) {
-      if (substrRegex.test(str)) {
-        matches.push(str);
-      }
-    });
-
-    cb(matches);
-  };
-};
-
-var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-];
-
-$('#the-basics .typeahead').typeahead({
-  hint: true,
-  highlight: true,
-  minLength: 1
-},
-{
-  name: 'states',
-  source: substringMatcher(states)
-});
+// $(document).ready(function() {
+//   $('#excelservice').on('change.states', function() {
+//     $("#states").toggle($(this).val() == 'telephonic');
+//   }).trigger('change.states');
+// // });
+//
+// $('#country').on('change', function() {
+//   if ( this.value == 'USA')
+//     $("#states").show();
+//   else
+//     $("#states").hide();
+// }).trigger("change"); // notice this line
